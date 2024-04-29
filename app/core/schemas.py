629caa9d6727b76c99020c7831a048c225132873
stavros <stv@pupil-labs.com>
2024-04-29 11:44:13 +0300

    Added custom base model for pydantic

---
+++ app/core/schemas.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pydantic import BaseModel, ConfigDict
+
+
+def convert_datetime_to_gmt(dt: datetime) -> str:
+    if not dt.tzinfo:
+        dt = dt.replace(tzinfo=ZoneInfo("UTC"))
+
+    return dt.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+
+class CustomModel(BaseModel):
+    model_config = ConfigDict(
+        json_encoders={datetime: convert_datetime_to_gmt},
+        populate_by_name=True,
+    )
