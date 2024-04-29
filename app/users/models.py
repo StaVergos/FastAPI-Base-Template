74d2235bc95e1c71aa3d1d281c5bcfc252c1b496
stavros <stv@pupil-labs.com>
2024-04-29 20:36:57 +0300

    Added created_at and updated_at on user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,6 @@
+import datetime
+from sqlalchemy import DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db import Base
@@ -12,3 +15,9 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column()
     is_active: Mapped[bool] = mapped_column()
     is_superuser: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
